c435bc87f4639e1cd6760d0e78f4d6c7de1dabe8
Githubshakil <shakilmahmud007@gmail.com>
2025-03-27 20:14:22 +0600

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let slide = document.querySelectorAll('.slide')
     let prevButton = document.querySelector('.prev')
     let nextButton = document.querySelector('.next')
+    let slideCount = document.querySelector('.slideCount')
     let dotsContainer = document.querySelector('.dots')
 
 
@@ -20,20 +21,45 @@ for(let i = 0; i < slide.length; i++){
    dotsContainer.appendChild(creatDot)
    
 }
+let dots = document.querySelectorAll('.dot')
 
 function updateSlider(index){
     if(index >= slide.length){
         currentSlide = 0;
     }else if(index < 0){
         currentSlide = slide.length - 1;
+    }else{
+        currentSlide = index;
     }
+    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+    dots.forEach((dot) => dot.classList.remove('active'));
+    dots[currentSlide].classList.add('active');
+    slideCount.innerHTML =`${currentSlide + 1} / ${slide.length}`;
 }
 
 nextButton.addEventListener('click',function(){
+// console.log('next');
+updateSlider(currentSlide + 1);
 
 })
+prevButton.addEventListener('click',function(){
+    // console.log('prev');
+    updateSlider(currentSlide - 1);
+    
+    })
+
+    dots.forEach((dot) =>{
+        dot.addEventListener('click',function(){
+            // console.log(typeof +dot.getAttribute('data-index'));
+            updateSlider(+dot.getAttribute('data-index'));
+            
+        })
+    })
+    setInterval(()=>{
+        updateSlider(currentSlide + 1);
+    },2500)
+    
+    })
 
 
-})
 
-42:55 secs
